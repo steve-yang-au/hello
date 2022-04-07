4a39ee52d7c61250a2f7dfd64dccba8c2747c9c2
Steve <steve.yang.au@gmail.com>
2022-04-07 16:41:24 +1000

    testing how to use git in IDE

---
+++ src/main/java/com/steve/hello/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.steve.hello.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("hello")
+public class HelloController {
+
+    @RequestMapping("hello")
+    String hello(){
+        return "hello";
+    }
+}
